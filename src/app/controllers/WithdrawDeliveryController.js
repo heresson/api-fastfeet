02ddc7bfef7c5788e9d6d8ac4833e5bb4c1a044c
heresson <heresson@gmail.com>
2020-04-15 18:20:05 -0300

    funcionalidade de retirada de produtos completa

--- src/app/controllers/WithdrawDeliveryController.js
+++ src/app/controllers/WithdrawDeliveryController.js
@@ -6,6 +6,9 @@ import {
   setHours,
   setMinutes,
   setSeconds,
+  getDate,
+  getMonth,
+  getYear,
 } from 'date-fns';
 import Deliveryman from '../models/Deliveryman';
 import Delivery from '../models/Delivery';
@@ -35,26 +38,64 @@ class WithdrawDeliveryController {
       return res.status(400).json({ error: 'Invalid deliveryId' });
     }
 
-    // check start_date between 8h00 - 18h00
     const { start_date } = req.body;
+
+    // permit only 5 deliveries ina a day
+    const numOfDeliveries = await Delivery.count({
+      where: {
+        start_date,
+        deliveryman_id: deliverymanId,
+      },
+    });
+    if (numOfDeliveries > 5) {
+      return res
+        .status(400)
+        .json({ error: 'You can only withdraw 5 products ina day' });
+    }
+
+    // check start_date between 8h00 - 18h00
     const start_date_ISO = parseISO(start_date);
+    const dateBeforePermitted = setSeconds(
+      setMinutes(
+        setHours(
+          new Date(
+            getYear(start_date_ISO),
+            getMonth(start_date_ISO),
+            getDate(start_date_ISO)
+          ),
+          7
+        ),
+        59
+      ),
+      59
+    );
+    const dateAfterPermitted = setSeconds(
+      setMinutes(
+        setHours(
+          new Date(
+            getYear(start_date_ISO),
+            getMonth(start_date_ISO),
+            getDate(start_date_ISO)
+          ),
+          18
+        ),
+        0
+      ),
+      0
+    );
 
     if (
-      isBefore(
-        start_date_ISO,
-        setSeconds(setMinutes(setHours(new Date(), 7), 59), 59)
-      ) ||
-      isAfter(
-        start_date_ISO,
-        setSeconds(setMinutes(setHours(new Date(), 18), 0), 0)
-      )
+      isBefore(start_date_ISO, dateBeforePermitted) ||
+      isAfter(start_date_ISO, dateAfterPermitted)
     ) {
       return res
         .status(400)
         .json({ error: 'start_date must be between 8h and 18h' });
     }
 
-    return res.json({ start_date });
+    const deliveryUpdated = await deliveryExists.update(req.body);
+
+    return res.json({ deliveryUpdated });
   }
 }
 export default new WithdrawDeliveryController();
